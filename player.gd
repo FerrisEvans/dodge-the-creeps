cbb64fa5cebc2fd3d674122af4ed001da9b7ac2a
Ferris <ferris.evans@outlook.com>
2025-02-07 16:29:51 +0800

    sprite movement

---
+++ player.gd
@@ -0,0 +1,36 @@
+extends Area2D
+
+# How fast the player will move (pixels/sec).
+@export var speed = 400
+# Size of the game window
+var screen_size
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	screen_size = get_viewport_rect().size
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	var velocity = Vector2.ZERO # The player's movement vector
+	if Input.is_action_pressed("move_right"):
+		velocity.x += 1
+	if Input.is_action_pressed("move_left"):
+		velocity.x -= 1	
+	if Input.is_action_pressed("move_right"):
+		velocity.x += 1
+	if Input.is_action_pressed("move_down"):
+		velocity.y += 1
+	if Input.is_action_pressed("move_up"):
+		velocity.y -= 1	
+	
+	if velocity.length() > 0:
+		velocity = velocity.normalized() * speed
+		# $ is shorthand for get_node()
+		# is the same as get_node("AnimatedSprite2D").play()
+		$AnimatedSprite2D.play()
+	else:
+		$AnimatedSprite2D.stop()
+	position += velocity * delta
+	position = position.clamp(Vector2.ZERO, screen_size)
+	
